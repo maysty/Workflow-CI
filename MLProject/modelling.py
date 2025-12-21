931118e5a21f1d3ba56cbf31d6ca78bccb118e8c
Ike Maya <ikemaya04@gmail.com>
2025-12-21 11:10:39 +0700

    Fix CI workflow

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -1,40 +1,24 @@
-import argparse
 import pandas as pd
-import mlflow
-import mlflow.sklearn
-
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
-from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+from sklearn.metrics import accuracy_score
 
+DATA_PATH = "MLProject/heart_preprocessing/heart_clean.csv"
 
-def main(dataset_path):
-    df = pd.read_csv(args.data_path)
+df = pd.read_csv(DATA_PATH)
 
-    X = df.drop(columns=["num"])
-    y = df["num"]
+X = df.drop(columns=["num"])
+y = df["num"]
 
-    X_train, X_test, y_train, y_test = train_test_split(
-        X, y, test_size=0.2, random_state=42, stratify=y
-    )
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
-    mlflow.sklearn.autolog()
+model = LogisticRegression(max_iter=1000)
+model.fit(X_train, y_train)
 
-    with mlflow.start_run():
-        model = LogisticRegression(max_iter=1000)
-        model.fit(X_train, y_train)
+preds = model.predict(X_test)
+acc = accuracy_score(y_test, preds)
 
-        y_pred = model.predict(X_test)
-
-        mlflow.log_metric("accuracy", accuracy_score(y_test, y_pred))
-        mlflow.log_metric("precision", precision_score(y_test, y_pred, average="weighted"))
-        mlflow.log_metric("recall", recall_score(y_test, y_pred, average="weighted"))
-        mlflow.log_metric("f1_score", f1_score(y_test, y_pred, average="weighted"))
-
-
-if __name__ == "__main__":
-    parser = argparse.ArgumentParser()
-    parser.add_argument("--data_path", type=str, required=True)
-    args = parser.parse_args()
-
-    main(args.data_path)
+print("Training success")
+print("Accuracy:", acc)
